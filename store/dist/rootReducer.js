b4eb93583f2603aa7a2324eba593076525a0d07a
Maestroharyor <haryor1josh@gmail.com>
2022-05-22 14:47:23 +0100

    Removed dist folders

--- store/dist/rootReducer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var redux_1 = require("redux");
-var reducer_1 = require("./theme/reducer");
-exports["default"] = redux_1.combineReducers({
-    theme: reducer_1["default"]
-});
